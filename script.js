3e51a30f7cc7842046d9e003faa4a1ec29d675d9
“Gpeter01” <240795083+Gpeter01@users.noreply.github.com>
2026-07-20 01:39:54 +0100

    Get and implement the size of the etch container

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict'
+function etchSize() {
+    const secondContainer = document.querySelector('.second-container');
+    const etch = document.querySelector('.etch');
+    const zContainer = document.querySelector('.z-container');
+    const secondContainerStyle = getComputedStyle(secondContainer);
+    const size = parseFloat(secondContainerStyle.width) - (parseFloat(secondContainerStyle.padding) * 2) + 'px';
+    etch.style.width = size;
+    etch.style.height = size;
+    zContainer.style.width = size;
+    zContainer.style.height = size;
+}
