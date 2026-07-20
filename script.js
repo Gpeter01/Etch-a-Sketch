0c3e150fcb1d04bb4d9fa2fcab9e97453ba056dc
“Gpeter01” <240795083+Gpeter01@users.noreply.github.com>
2026-07-20 01:44:47 +0100

    Find computed width and create the appropriate etch pad

--- script.js
+++ script.js
@@ -11,3 +11,31 @@ function etchSize() {
     zContainer.style.height = size;
 }
 etchSize();
+function findWidth(numberPercolumn) {
+    const etch = document.querySelector('.etch');
+    const computedStylesOfContainer = getComputedStyle(etch);
+    const sizeOfBox = `${(parseFloat(computedStylesOfContainer.width) - (parseFloat(computedStylesOfContainer.borderWidth) * 2)) / numberPercolumn}px`;
+    return sizeOfBox;
+}
+function makeDivs(numOfDivs, widthOfDivs, heightOfDivs, numberPerRow) {
+    for (let i = 1; i <= numOfDivs; i++) {
+        const miniDiv = document.createElement('div');
+        miniDiv.classList.add(`miniDiv${i}`);
+        miniDiv.style.width = widthOfDivs;
+        miniDiv.style.height = heightOfDivs;
+        miniDiv.style.opacity = 0;
+        const etch = document.querySelector('.etch');
+        etch.appendChild(miniDiv);
+    }
+    getBorderRadius(numberPerRow, numOfDivs);
+}
+function getBorderRadius(numberOfDivs, squareOfNumberOfDivs) {
+    const miniDivTopLeftCorner = document.querySelector(`.miniDiv1`);
+    miniDivTopLeftCorner.style.borderTopLeftRadius = '2rem';
+    const miniDivTopRightCorner = document.querySelector(`.miniDiv${numberOfDivs}`);
+    miniDivTopRightCorner.style.borderTopRightRadius = '2rem';
+    const miniDivBottomLeftCorner = document.querySelector(`.miniDiv${squareOfNumberOfDivs - numberOfDivs + 1}`);
+    miniDivBottomLeftCorner.style.borderBottomLeftRadius = '2rem';
+    const miniDivBottomRightCorner = document.querySelector(`.miniDiv${squareOfNumberOfDivs}`);
+    miniDivBottomRightCorner.style.borderBottomRightRadius = '2rem';
+}
